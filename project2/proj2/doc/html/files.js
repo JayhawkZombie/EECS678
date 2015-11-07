e8dc8be489b689d9f077f3bc803c6209b47d0155
Kurt Slagle <jayhawkzombie@yahoo.com>
2015-11-06 19:05:40 -0600

    Added missing folders

---
+++ project2/proj2/doc/html/files.js
@@ -0,0 +1,7 @@
+var files =
+[
+    [ "libpriqueue/libpriqueue.c", "libpriqueue_8c.html", "libpriqueue_8c" ],
+    [ "libpriqueue/libpriqueue.h", "libpriqueue_8h.html", "libpriqueue_8h" ],
+    [ "libscheduler/libscheduler.c", "libscheduler_8c.html", "libscheduler_8c" ],
+    [ "libscheduler/libscheduler.h", "libscheduler_8h.html", "libscheduler_8h" ]
+];
---
+++ project2/proj2/doc/html/pages.js
@@ -0,0 +1,7 @@
+var pages =
+[
+    [ "Doxygen", "doxygen.html", null ],
+    [ "Acknowledgement", "ack.html", null ],
+    [ "Compare Function", "comparer-page.html", null ],
+    [ "Examples", "examplesPage.html", "examplesPage" ]
+];
