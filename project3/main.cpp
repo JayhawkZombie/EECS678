6a3921b1a4494beb46cba3d91d3729bd54d5efdf
Kurt Slagle <kurt_slagle@yahoo.com>
2015-12-06 23:23:19 -0600

    Update main.cpp

--- project3/main.cpp
+++ project3/main.cpp
@@ -1,24 +1,24 @@
 /**
- * Buddy Allocator
- *
- * For the list library usage, see http://www.mcs.anl.gov/~kazutomo/list/
- */ 
+* Buddy Allocator
+*
+* For the list library usage, see http://www.mcs.anl.gov/~kazutomo/list/
+*/
 
 /**************************************************************************
- * Conditional Compilation Options
- **************************************************************************/
+* Conditional Compilation Options
+**************************************************************************/
 #define USE_DEBUG 0
 
 /**************************************************************************
- * Included Files
- **************************************************************************/
+* Included Files
+**************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
 /**************************************************************************
- * Public Definitions
- **************************************************************************/
+* Public Definitions
+**************************************************************************/
 #define MIN_ORDER 12
 #define MAX_ORDER 20
 
@@ -48,28 +48,28 @@ typedef unsigned int bool;
 #endif
 
 /**************************************************************************
- * Public Types
- **************************************************************************/
+* Public Types
+**************************************************************************/
 typedef struct {
 	struct list_head list;
 	/* TODO: DECLARE NECESSARY MEMBER VARIABLES */
 } page_t;
 
 /**************************************************************************
- * Global Variables
- **************************************************************************/
+* Global Variables
+**************************************************************************/
 /* free lists*/
-struct list_head free_area[MAX_ORDER+1];
+struct list_head free_area[MAX_ORDER + 1];
 
 /* memory area */
-char g_memory[1<<MAX_ORDER]; 
+char g_memory[1 << MAX_ORDER];
 
 /* page structures */
-page_t g_pages[(1<<MAX_ORDER)/PAGE_SIZE]; 
+page_t g_pages[(1 << MAX_ORDER) / PAGE_SIZE];
 
 /**************************************************************************
- * Public Function Prototypes
- **************************************************************************/
+* Public Function Prototypes
+**************************************************************************/
 
 void buddy_init();
 void *buddy_alloc(int size);
@@ -79,21 +79,21 @@ void buddy_dump();
 void buddy_dump2();
 
 /**************************************************************************
- * Local Functions 
- **************************************************************************/
+* Local Functions
+**************************************************************************/
 
 /**
- * Initialize the buddy system
- */
+* Initialize the buddy system
+*/
 void buddy_init()
 {
-	int i; 
-	int n_pages = (1<<MAX_ORDER) / PAGE_SIZE;
+	int i;
+	int n_pages = (1 << MAX_ORDER) / PAGE_SIZE;
 	for (i = 0; i < n_pages; i++) {
 		/* TODO: INITIALIZE PAGE STRUCTURES */
 		INIT_LIST_HEAD(&g_pages[i].list);
 	}
-	
+
 	/* initialize freelist */
 	for (i = MIN_ORDER; i <= MAX_ORDER; i++) {
 		INIT_LIST_HEAD(&free_area[i]);
@@ -104,55 +104,55 @@ void buddy_init()
 }
 
 /**
- * Allocate a memory block.
- *
- * @param size size in bytes
- * @return memory block address
- */
+* Allocate a memory block.
+*
+* @param size size in bytes
+* @return memory block address
+*/
 
-void * buddy_alloc( int size ) //WORKING
+void * buddy_alloc(int size) //WORKING
 {
-     int i;
+	int i;
 
-     for (i = 0; (1 << i)  < size; i++);
+	for (i = 0; (1 << i) < size; i++);
 
-     if (i > MAX_ORDER) {
-          return NULL;
-     }
-     else if (!list_empty(&free_area[i])) {
-          struct list_head *block;
-          block = &free_area[i];
-          list_del_init(&free_area[i]);
-          return block;
-     }
-     else {
-    	  struct list_head *block, *buddy;
-          block = buddy_alloc(1 << ( i + 1 ));
+	if (i > MAX_ORDER) {
+		return NULL;
+	}
+	else if (!list_empty(&free_area[i])) {
+		struct list_head *block;
+		block = &free_area[i];
+		list_del_init(&free_area[i]);
+		return block;
+	}
+	else {
+		struct list_head *block, *buddy;
+		block = buddy_alloc(1 << (i + 1));
 
-          if (block != NULL) {
-              buddy = ((struct list_head *)( ((int)block) ^ (1 << i))); //the macro given was SEG FAULT for BUDDY_ADDR(block, i)
-              list_add(buddy, &free_area[i]);
-          }
+		if (block != NULL) {
+			buddy = ((struct list_head *)(((int)block) ^ (1 << i))); //the macro given was SEG FAULT for BUDDY_ADDR(block, i)
+			list_add(buddy, &free_area[i]);
+		}
 
-          return block;
-     }
+		return block;
+	}
 }
 
 /**
- * Free an allocated memory block.
- *
- * @param addr memory block address to be freed
- */
+* Free an allocated memory block.
+*
+* @param addr memory block address to be freed
+*/
 void buddy_free(void *addr) //NOT WORKING
 {
 
 }
 
 /**
- * Print the buddy system status---order oriented
- *
- * print free pages in each order.
- */
+* Print the buddy system status---order oriented
+*
+* print free pages in each order.
+*/
 void buddy_dump()
 {
 	int o;
@@ -162,7 +162,7 @@ void buddy_dump()
 		list_for_each(pos, &free_area[o]) {
 			cnt++;
 		}
-		printf("%d:%dK ", cnt, (1<<o)/1024);
+		printf("%d:%dK ", cnt, (1 << o) / 1024);
 	}
 	printf("\n");
 }
@@ -173,25 +173,25 @@ int main(int argc, char *argv[])
 	char *A, *B, *C, *D;
 
 	buddy_init();
- 	
-	A = buddy_alloc(80*1024); //80kb
+
+	A = buddy_alloc(80 * 1024); //80kb
 	buddy_dump();
 
-	B = buddy_alloc(60*1024); //60kb
+	B = buddy_alloc(60 * 1024); //60kb
 	buddy_dump();
 
-	C = buddy_alloc(80*1024); //80kb
+	C = buddy_alloc(80 * 1024); //80kb
 	buddy_dump();
 
 	buddy_free(A);
 	buddy_dump();
 
-	D = buddy_alloc(32*1024); //32kb
+	D = buddy_alloc(32 * 1024); //32kb
 	buddy_dump();
-	
+
 	buddy_free(B);
 	buddy_dump();
-	
+
 	buddy_free(D);
 	buddy_dump();
 
